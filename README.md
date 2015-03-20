4d673dcba60ec6d64995f0aad3ecd1df894f7ec2
Javier Godoy <rotvulpix@gmail.com>
2015-03-20 00:33:03 -0300

    Inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+WebPay para Laravel5
+==================
+
+Módulo WebPay para Laravel5. Por el momento no es funcional. 
+Hermano de [transbank-symfony2](http://github.com/rotvulpix/transbank-symfony2 "transbank-symfony2")
+
+###Coming Soon!
---
+++ src/ServiceProvider.php
@@ -0,0 +1,79 @@
+<?php namespace rotvulpix\transbank;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+
+        $this->handleConfigs();
+        // $this->handleMigrations();
+        // $this->handleViews();
+        // $this->handleTranslations();
+        // $this->handleRoutes();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+
+        // Bind any implementations.
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+
+        return [];
+    }
+
+    private function handleConfigs() {
+
+        $configPath = __DIR__ . '/../config/transbank.php';
+
+        $this->publishes([$configPath => config_path('transbank.php')]);
+
+        $this->mergeConfigFrom($configPath, 'transbank');
+    }
+
+    private function handleTranslations() {
+
+        $this->loadTranslationsFrom('transbank', __DIR__.'/../lang');
+    }
+
+    private function handleViews() {
+
+        $this->loadViewsFrom('transbank', __DIR__.'/../views');
+
+        $this->publishes([__DIR__.'/../views' => base_path('resources/views/vendor/transbank')]);
+    }
+
+    private function handleMigrations() {
+
+        $this->publishes([__DIR__ . '/../migrations' => base_path('database/migrations')]);
+    }
+
+    private function handleRoutes() {
+
+        include __DIR__.'/../routes.php';
+    }
+}
