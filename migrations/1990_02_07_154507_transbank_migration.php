2b059f8d1776c1d4f1a22619f87f4103f3b144af
Javier Godoy <rotvulpix@gmail.com>
2015-03-20 22:43:44 -0300

    Modelo y Clase, tal cual como en Symfony

--- migrations/1990_02_07_154507_transbank_migration.php
+++ migrations/1990_02_07_154507_transbank_migration.php
@@ -12,7 +12,24 @@ class TransbankMigration extends Migration {
 	 */
 	public function up()
 	{
-		
+		Schema::create('webpay_log', function($table) {
+			$table->increments('id');
+			$table->integer('tipo_transaccion');
+			$table->integer('monto');
+			$table->string('orden_compra');
+			$table->string('sesion');
+			$table->integer('estado');
+			$table->integer('respuesta');
+			$table->string('codigo_autorizacion');
+			$table->integer('numero_tarjeta');
+			$table->datetime('fecha_contable');
+			$table->datetime('fecha_transaccion');
+			$table->datetime('fecha_inicio_transaccion');
+			$table->integer('transaccion');
+			$table->string('tipo_pago');
+			$table->integer('cuotas');
+			$table->string('vci');
+			$table->string('mac');
 	}
 
 	/**
@@ -22,7 +39,7 @@ class TransbankMigration extends Migration {
 	 */
 	public function down()
 	{
-		
+		Schema::drop('webpay_log');
 	}
 
 }
---
+++ src/Models/WebPayLog.php
@@ -0,0 +1,9 @@
+<?php namespace rotvulpix\romulus\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class WebPayLog extends Model {
+
+    protected $table = 'webpay_log';
+    public $timestamps = false;
+
+}
