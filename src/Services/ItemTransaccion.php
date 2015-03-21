cdfba2fd0dca41ad87a29da6ffc7936f0fba5ed5
Javier Godoy <rotvulpix@gmail.com>
2015-03-20 22:49:27 -0300

    Services

---
+++ src/Services/ItemTransaccion.php
@@ -0,0 +1,42 @@
+<?php
+namespace rotvulpix\transbank\Services;
+class ItemTransaccion
+{
+	// Definición de Propiedades
+	private $cantidad, $unitario, $nombre;
+	// Definición por defecto. Cantidad por defecto = 1
+	function __construct($unitario, $nombre, $cantidad = 1) {
+		$this->cantidad = $cantidad;
+		$this->unitario = $unitario;
+		$this->nombre = $nombre;
+	}
+	
+	// Obtener Cantidad de Ítems
+	public function getCantidad() {
+		return $this->cantidad;
+	}
+	// Obtener Precio Unitario
+	public function getUnitario() {
+		return $this->unitario;
+	}
+	// Obtener Nombre de Ítem
+	public function getNombre() {
+		return $this->nombre;
+	}
+	// Obtener Precio Total
+	public function getTotalItem() {
+		return $this->cantidad * $this->unitario;
+	}
+	// Setear Cantidad
+	public function setCantidad($cantidad) {
+		$this->cantidad = $cantidad;
+	}
+	// Setear Precio Unitario
+	public function setUnitario($unitario) {
+		$this->unitario = $unitario;
+	}
+	// Setear Nombre
+	public function setNombre($nombre) {
+		$this->nombre = $nombre;
+	}
+}
---
+++ src/Services/Transaccion.php
@@ -0,0 +1,34 @@
+<?php
+namespace rotvulpix\transbank\Services;
+class Transaccion
+{
+	// Definición de Propiedades
+	private $items = Array();
+	// Obtenemos Total de Transacción
+	function getTotalTransaccion() {
+		$suma = 0;
+		// Por cada Ítem
+		foreach($this->items as $item) {
+			// Añadimos Total del Ítem
+			$suma += $item->getTotalItem();
+		}
+		// Retornamos el Valor
+		return $suma;
+	}
+	function getNeto() {
+		$total = $this->getTotalTransaccion();
+		$neto = $total / 1.19;
+		return round($neto);
+	}
+	function getIva() {
+		return $this->getTotalTransaccion() - $this->getNeto();
+	}
+	// Obtener Ítems de la Transacción
+	function getItems() {
+		return $this->items;
+	}
+	// Agregar Ítem a la transacción
+	function addItem(ItemTransaccion $item) {
+		$this->items[] = $item;
+	}
+}
