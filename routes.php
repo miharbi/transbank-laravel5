e26d6d5ab95d5149945dc7ec931f18752fe4f980
Javier Godoy <rotvulpix@gmail.com>
2015-03-21 00:57:08 -0300

    Routes

--- routes.php
+++ routes.php
@@ -3,3 +3,11 @@
 /**
  * Your package routes would go here
  */
+
+Route::group(['prefix' => 'webpay'], function() {
+	// Route::get('Compra', ['as' => 'tbk_demo', 'uses' => '\rotvulpix\transbank\Controllers\DemoController@Compra']);
+	// Route::get('webpay', ['as' => 'tbk_demo_checkout', 'uses' => '\rotvulpix\transbank\Controllers\DemoController@WebPay']);
+	Route::post('cierre', ['as' => 'tbk_cierre', 'uses' => '\rotvulpix\transbank\Controllers\WebPayController@cierre']);
+	Route::post('exito', ['as' => 'tbk_exito', 'uses' => '\rotvulpix\transbank\Controllers\WebPayController@exito']);
+	Route::post('fracaso', ['as' => 'tbk_fracaso', 'uses' => '\rotvulpix\transbank\Controllers\WebPayController@fracaso']);
+});
